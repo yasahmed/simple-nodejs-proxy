326af68dfdec539d07e959eee338a1b4b5cb1c42
JNAH Ahmed <eessaouira@gmail.com>
2021-09-17 16:01:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
+
+    var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({target:'0.0.0.0:8080'}).listen(8180); // See (†)
+httpProxy.createProxyServer({target:host+':'+port}).listen(8180); // See (†)
  
 //
 // Create your target server
@@ -12,4 +16,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
   res.end();
-}).listen(8080);
+}).listen(port);
