9619c3da6e2aaf159ddcff9efa95653f566d4a8c
JNAH Ahmed <eessaouira@gmail.com>
2021-09-17 15:45:57 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:8080'}).listen(8180); // See (†)
+ 
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(8080);
